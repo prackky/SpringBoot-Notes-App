cb5c77cf842dafb36b316a9c4a2c2ac08e0f510a
Prakhar Rastogi <rastogi.prakhar@live.com>
2018-05-23 14:55:36 +0530

    redis consts updated

--- src/main/java/com/sbnote/constants/RedisConstants.java
+++ src/main/java/com/sbnote/constants/RedisConstants.java
@@ -2,8 +2,8 @@ package com.sbnote.constants;
 
 public class RedisConstants {
 
-	public static final String REDIS_HOST = "localhost";
-	public static final int REDIS_PORT = 6379;
+	public static final String REDIS_HOST = "redis-18315.c1.ap-southeast-1-1.ec2.cloud.redislabs.com";
+	public static final int REDIS_PORT = 18315;
 	public static final String REDIS_DB_KEY = "USERID_TOKEN_MAPPING";
 	public static final int REDIS_KEY_EXPIRATION_SECONDS = 1800;
 }
