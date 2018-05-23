f41205cad82b675af9859cda948b483c148f3357
Prakhar <rastogi.prakhar@live.com>
2018-05-23 22:52:07 +0530

    unused import removed

--- src/main/java/com/sbnote/controller/UserController.java
+++ src/main/java/com/sbnote/controller/UserController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sbnote.model.JwtResponse;
 import com.sbnote.model.User;
